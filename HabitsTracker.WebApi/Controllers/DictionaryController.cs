11403bcadf00c7a37460ba5886678576a5c1745a
diniyuha <diniyuha@gmail.com>
2023-04-20 18:55:02 +0600

    add controllers

---
+++ HabitsTracker.WebApi/Controllers/DictionaryController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using HabitsTracker.Logic.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HabitsTracker.WebApi.Controllers
+{
+    public class DictionaryController : Controller
+    {
+        private readonly IDictionaryService _dictionaryService;
+        
+        public DictionaryController(IDictionaryService dictionaryService)
+        {
+            _dictionaryService = dictionaryService;
+        }
+        
+        //TODO сделать на основании DictionaryService
+    }
+}
---
+++ HabitsTracker.WebApi/Controllers/HabitController.cs
@@ -0,0 +1,19 @@
+﻿using HabitsTracker.Logic.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HabitsTracker.WebApi.Controllers
+{
+    public class HabitController : Controller
+    {
+        private readonly IHabitService _habitService;
+        private readonly IDictionaryService _dictionaryService;
+        
+        public HabitController(IHabitService habitService, IDictionaryService dictionaryService)
+        {
+            _habitService = habitService;
+            _dictionaryService = dictionaryService;
+        }
+        
+        //TODO сделать на основании HabitService
+    }
+}
